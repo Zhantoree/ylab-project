3c9ae253462c0e3a1c5b9793b5db54b56ab0ad27
unknown <drug2334@gmail.com>
2023-11-19 20:31:05 +0600

    login page is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import {Route, Routes} from "react-router-dom";
+import LoginForm from "./components/Pages/LoginForm/LoginForm";
+import RegisterForm from "./components/Pages/RegisterForm/RegisterForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<LoginForm/>}/>
+        <Route path='/register' element={<RegisterForm/>}/>
+      </Routes>
     </div>
   );
 }
